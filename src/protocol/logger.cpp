c7cd0671472ff547843fdb6f64e12b8b1762e37c
Marcin Kolny <marcin.kolny@gmail.com>
2015-09-02 20:18:04 +0200

    display date and time in logger

--- src/protocol/logger.cpp
+++ src/protocol/logger.cpp
@@ -1,10 +1,17 @@
 #include "logger.h"
 
+#include <ctime>
 #include <iostream>
 
 using namespace Wimf;
 
 void Logger::log (const std::string& message)
 {
-	std::cout << message << std::endl;
+	time_t now = time(0);
+	tm tstruct;
+	char buf[80];
+	tstruct = *localtime(&now);
+	strftime(buf, sizeof(buf), "%Y-%m-%d %X", &tstruct);
+
+	std::cout << "[" << buf << "] " << message << std::endl;
 }
