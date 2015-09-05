9c3d40b569094fce912fb7361f4d33d31b9042d8
Marcin Kolny <marcin.kolny@gmail.com>
2015-09-05 21:33:41 +0200

    android-client: selecting avatar implemented

--- src/android-client/app/src/main/java/eu/cookandcommit/wimf/wimf/StartUpActivity.java
+++ src/android-client/app/src/main/java/eu/cookandcommit/wimf/wimf/StartUpActivity.java
@@ -2,6 +2,9 @@ package eu.cookandcommit.wimf.wimf;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -10,9 +13,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class StartUpActivity extends AppCompatActivity {
     private EditText nickEditText;
+    private ImageView avatarImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,27 +25,74 @@ public class StartUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start_up);
 
         nickEditText = (EditText) findViewById(R.id.nickEditText);
+        assert nickEditText != null;
 
-        final Button button = (Button) findViewById(R.id.joinButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (TextUtils.isEmpty(nickEditText.getText())) {
-                    new AlertDialog.Builder(StartUpActivity.this)
-                            .setTitle("Invalid nick")
-                            .setMessage("Nick name cannot be empty")
-                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    // do nothing, just close this dialog
-                                }
-                            })
-                            .setIcon(android.R.drawable.ic_dialog_info)
-                            .show()
-                            .setCanceledOnTouchOutside(true);
+        avatarImageView = (ImageView) findViewById(R.id.avatarImageView);
+        assert avatarImageView != null;
+    }
+
+    public void join_joinButton_click(View v) {
+        if (TextUtils.isEmpty(nickEditText.getText())) {
+            showEmptyNickWarning();
+        }
+    }
+
+    public void pickAvatar_avatarImageView_click(View v) {
+        new AlertDialog.Builder(StartUpActivity.this)
+                .setTitle("Select avatar source")
+                .setItems(R.array.avatar_source_array, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent pickAvatar = null;
+                        switch (which) {
+                            case 0:  // camera
+                                pickAvatar = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                                break;
+                            case 1:  // gallery
+                                pickAvatar = new Intent(Intent.ACTION_PICK,
+                                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                break;
+                        }
+                        if (pickAvatar != null) {
+                            startActivityForResult(pickAvatar, which);
+                        }
+                    }
+                }).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        Uri avatar = null;
+        switch (requestCode) {
+            case 0:
+                if (resultCode == RESULT_OK) {
+                    avatar = imageReturnedIntent.getData();
                 }
-            }
-        });
 
+                break;
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    avatar = imageReturnedIntent.getData();
+                }
+                break;
+        }
+
+        avatarImageView.setImageURI(avatar);
+    }
+
+    private void showEmptyNickWarning() {
+        new AlertDialog.Builder(StartUpActivity.this)
+                .setTitle("Invalid nick")
+                .setMessage("Nick can not be empty")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing, just close this dialog
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .show()
+                .setCanceledOnTouchOutside(true);
     }
 
     @Override
